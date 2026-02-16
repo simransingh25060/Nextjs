b37032f31c37e7018830fe263c58224abecbd284
Simran Singh <simransingh25060@gmail.com>
2026-02-17 02:36:02 +0530

    OTP verification and unique username check

--- helpers/sendVerificationEmail.ts
+++ helpers/sendVerificationEmail.ts
@@ -10,7 +10,7 @@ export async function sendVerificationEmail(
     try {
         await resend.emails.send({
   from: "onboarding@resend.dev",
-  to: "email",
+  to: email,
   subject: "Mystry message | Verification code",
   react: VerificationEmail({username, otp: verifyCode}),
 });
